3f900037a27f8bd4fb729ac899316553fd03d571
yezanee <rbfrk02@naver.com>
2026-03-09 20:08:28 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/passroutebackend/PassrouteBackendApplication.java
@@ -0,0 +1,13 @@
+package passroutebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassrouteBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PassrouteBackendApplication.class, args);
+  }
+
+}
